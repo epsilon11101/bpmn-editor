187f141b10a2ad5632868453ebcc15e66845da50
epsilon11101 <28758777+epsilon11101@users.noreply.github.com>
2025-08-14 10:44:58 -0600

    Refactor createBpmnModelerEngine to use diagramEngine class for improved structure and maintainability

--- src/ports/engine.ts
+++ src/ports/engine.ts
@@ -1,6 +1,10 @@
+// create class that implements diagramengine
+//create function that return bpmnengine
+//Create singlenton to create only one bpmn engine
+
 import type { ModdleExtension } from "bpmn-js/lib/BaseViewer";
 import BpmnModeler from "bpmn-js/lib/Modeler";
-import type { Capabilities, DiagramEngine } from "./diagramApi";
+import type { Capabilities, DiagramApi, DiagramEngine } from "./diagramApi";
 import { normalizeXML } from "../utils/utils";
 
 type EngineConfig = {
@@ -11,37 +15,44 @@ type EngineConfig = {
   eventResizingEnabled?: boolean;
 };
 
+class diagramEngine implements DiagramApi {
+  private modeler: BpmnModeler | null = null;
+  private cfg: EngineConfig = {};
+  constructor(cfg: EngineConfig) {
+    this.cfg = cfg;
+  }
+
+  async mount(container) {
+    if (this.modeler) return;
+    this.modeler = new BpmnModeler({
+      container: container,
+      additionalModules: this.cfg?.additionalModules,
+      moddleExtensions: this.cfg?.moddleExtensions || {},
+      taskResizingEnabled: this.cfg?.moddleExtensions || false,
+      eventResizingEnabled: this.cfg?.eventResizingEnabled || false,
+    });
+  }
+  destroy() {
+    try {
+      this.modeler?.destroy?.();
+    } finally {
+      this.modeler = null;
+    }
+  }
+  getCapabilities(): Capabilities {
+    return { editable: true };
+  }
+  async exportDiagram() {
+    const data = await this.modeler?.saveXML();
+    const currentXML = normalizeXML(data?.xml);
+    console.log(currentXML);
+  }
+  async exportImage() {
+    const data = await this.modeler?.saveSVG();
+    console.log(data);
+  }
+}
+
 export function createBpmnModelerEngine(cfg: EngineConfig): DiagramEngine {
-  let modeler: BpmnModeler | null = null;
-  return {
-    async mount(container) {
-      if (modeler) return;
-      modeler = new BpmnModeler({
-        container: container,
-        additionalModules: cfg?.additionalModules,
-        moddleExtensions: cfg?.moddleExtensions || {},
-        taskResizingEnabled: cfg?.moddleExtensions || false,
-        eventResizingEnabled: cfg?.eventResizingEnabled || false,
-      });
-    },
-    destroy() {
-      try {
-        modeler?.destroy?.();
-      } finally {
-        modeler = null;
-      }
-    },
-    getCapabilities(): Capabilities {
-      return { editable: true };
-    },
-    async exportDiagram() {
-      const data = await modeler?.saveXML();
-      const currentXML = normalizeXML(data?.xml);
-      console.log(currentXML);
-    },
-    async exportImage() {
-      const data = await modeler?.saveSVG();
-      console.log(data);
-    },
-  };
+  return new diagramEngine(cfg);
 }
