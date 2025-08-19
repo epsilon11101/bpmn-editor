c7319ef8beaa7844785e4cbece949c0c5fd9fdfe
epsilon11101 <28758777+epsilon11101@users.noreply.github.com>
2025-08-19 16:33:36 -0600

    Refactor App component to integrate DiagramService and initialize BPMN modeler

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import "./App.css";
+import { DiagramService } from "./ports/diagramService";
+import { createBpmnModelerEngine } from "./ports/engine";
+import ixModdle from "./lib/TBpmn/moddleDefinition.json";
+import TBPMNModel from "./lib/TBpmn";
+import { DiagramContext } from "./diagram/DiagramContext";
+import DiagramComponent from "./diagram/DiagramComponent";
+
+const bpmnEngine = createBpmnModelerEngine({
+  additionalModules: [TBPMNModel],
+  moddleExtensions: {
+    ix: ixModdle,
+  },
+  taskResizingEnabled: true,
+  eventResizingEnabled: true,
+});
+const diagramService = new DiagramService(bpmnEngine);
 
 function App() {
-  return <div></div>;
+  return (
+    <DiagramContext.Provider value={diagramService}>
+      <DiagramComponent />
+    </DiagramContext.Provider>
+  );
 }
 
 export default App;
