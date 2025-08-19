10920a6f414e5b874122210ca575398f04ba06ee
epsilon11101 <28758777+epsilon11101@users.noreply.github.com>
2025-08-19 16:33:56 -0600

    Add CSS imports for BPMN rendering and define an empty diagram template

--- src/ports/engine.ts
+++ src/ports/engine.ts
@@ -6,6 +6,9 @@ import type { ModdleExtension } from "bpmn-js/lib/BaseViewer";
 import BpmnModeler from "bpmn-js/lib/Modeler";
 import type { Capabilities, DiagramApi, DiagramEngine } from "./diagramApi";
 import { normalizeXML } from "../utils/utils";
+import "bpmn-js/dist/assets/diagram-js.css";
+import "bpmn-js/dist/assets/bpmn-js.css";
+import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
 
 type EngineConfig = {
   // eslint-disable-next-line
@@ -14,6 +17,16 @@ type EngineConfig = {
   taskResizingEnabled?: boolean;
   eventResizingEnabled?: boolean;
 };
+
+const emptyDiagram = `
+  <?xml version="1.0" encoding="UTF-8"?>
+<bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" id="Definitions_02dbtyp" targetNamespace="http://bpmn.io/schema/bpmn" exporter="bpmn-js (https://demo.bpmn.io)" exporterVersion="18.6.1">
+  <bpmn:process id="Process_07itzzv" isExecutable="false" />
+  <bpmndi:BPMNDiagram id="BPMNDiagram_1">
+    <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_07itzzv" />
+  </bpmndi:BPMNDiagram>
+</bpmn:definitions>
+  `;
 
 class diagramEngine implements DiagramApi {
   private modeler: BpmnModeler | null = null;
@@ -24,13 +37,20 @@ class diagramEngine implements DiagramApi {
 
   async mount(container) {
     if (this.modeler) return;
+
     this.modeler = new BpmnModeler({
       container: container,
       additionalModules: this.cfg?.additionalModules,
       moddleExtensions: this.cfg?.moddleExtensions || {},
-      taskResizingEnabled: this.cfg?.moddleExtensions || false,
+      taskResizingEnabled: this.cfg?.taskResizingEnabled || false,
       eventResizingEnabled: this.cfg?.eventResizingEnabled || false,
     });
+    try {
+      // empty diagram
+      await this.modeler.importXML(emptyDiagram);
+    } catch (err) {
+      console.error("reading error", err);
+    }
   }
   destroy() {
     try {
