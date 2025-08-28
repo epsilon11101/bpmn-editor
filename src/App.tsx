5c785e89a7374c2fce17df81e923594cdc93974e
epsilon11101 <28758777+epsilon11101@users.noreply.github.com>
2025-08-28 13:46:48 -0600

    Refactor App component to use DiagramProvider and remove DiagramContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import "./App.css";
-import { DiagramService } from "./ports/diagramService";
-import { createBpmnModelerEngine } from "./ports/engine";
-import ixModdle from "./lib/TBpmn/moddleDefinition.json";
-import TBPMNModel from "./lib/TBpmn";
-import { DiagramContext } from "./diagram/DiagramContext";
-import DiagramComponent from "./diagram/DiagramComponent";
 
-const bpmnEngine = createBpmnModelerEngine({
-  additionalModules: [TBPMNModel],
-  moddleExtensions: {
-    ix: ixModdle,
-  },
-  taskResizingEnabled: true,
-  eventResizingEnabled: true,
-});
-const diagramService = new DiagramService(bpmnEngine);
+import DiagramComponent from "./diagram/DiagramComponent";
+import { DiagramProvider } from "./diagram/DiagramProvider";
 
 function App() {
   return (
-    <DiagramContext.Provider value={diagramService}>
+    <DiagramProvider>
       <DiagramComponent />
-    </DiagramContext.Provider>
+    </DiagramProvider>
   );
 }
 
